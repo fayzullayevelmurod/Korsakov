a81174013e4ddc1cd2b2822c7af3d2b5faf101c5
SardorQobilov <sardorqobilov555@gmail.com>
2024-07-23 22:28:22 +0500

    Преимущества комплекса section finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,18 @@ module.exports = {
     extend: {
       fontFamily: {
         ReformaGroteskLight: [
-          "ReformaGroteskDemiC",
-          "Montserrat",
+          '"ReformaGroteskDemiC"',
+          '"Montserrat"',
           "sans-serif",
         ],
         Montserrat: ['"Montserrat"', "sans-serif"],
+        Muller: ['"Muller"', "sans-serif"],
       },
       colors: {
         "graphite-gray": "#313245",
         "light-brown": "#B99984",
         "light-gray": "#313245B2",
+        "light-white": "#FFFFFFB2",
       },
       lineHeight: {
         6.5: "26px",
