487b50a893185b02f3738589bb38db5057cbd380
SardorQobilov <sardorqobilov555@gmail.com>
2024-07-24 11:23:47 +0500

    swiper mouse not working

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,28 @@ module.exports = {
     extend: {
       fontFamily: {
         ReformaGroteskLight: [
-          '"ReformaGroteskDemiC"',
+          '"Reforma Grotesk Light W08 Rg"',
+          // '"ReformaGroteskDemiC"',
+          '"Montserrat"',
+          "sans-serif",
+        ],
+        ReformaGroteskMedium: [
+          '"Reforma Grotesk W01 Medium"',
+          // '"ReformaGroteskDemiC"',
           '"Montserrat"',
           "sans-serif",
         ],
         Montserrat: ['"Montserrat"', "sans-serif"],
-        Muller: ['"Muller"', "sans-serif"],
+        // Muller: ['"Muller"', "sans-serif"],
+        Muller: ['"Muller-Regular18"', "sans-serif"],
+        MullerLight: ['"Muller W00 Light"', "sans-serif"],
       },
       colors: {
         "graphite-gray": "#313245",
         "light-brown": "#B99984",
         "light-gray": "#313245B2",
         "light-white": "#FFFFFFB2",
-        "border-color": "#B9998469"
+        "border-color": "#B9998469",
       },
       lineHeight: {
         6.5: "26px",
