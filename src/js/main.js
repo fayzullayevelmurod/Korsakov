00eddc947d66775cd6eeb76b8ccad875fe8488e3
SardorQobilov <sardorqobilov555@gmail.com>
2024-07-27 12:19:50 +0500

    s

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,44 @@
 // header
+
+// headerMenu
 const headerMenu = document.querySelector(".header-menu");
 const closeBtn = document.querySelector(".close-menu__btn");
 const openMenuBtn = document.querySelector(".open-menu__btn");
+const menuLinks = document.querySelectorAll(".header-menu a");
+
+// Menyuni ochish
 openMenuBtn.addEventListener("click", () => {
   headerMenu.classList.remove("-translate-x-full");
+  document.body.classList.add("overflow-hidden", "pr-[5px]");
 });
+
+// Menyuni yopish
 closeBtn.addEventListener("click", () => {
   headerMenu.classList.add("-translate-x-full");
+  document.body.classList.remove("overflow-hidden", "pr-[5px]");
+});
+
+// Menyudagi <a> teglarga bosilganda menyuni yopish
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    headerMenu.classList.add("-translate-x-full");
+    document.body.classList.remove("overflow-hidden", "pr-[5px]");
+  });
+});
+
+// Ekranda istalgan joyga bosilganida, agar bosilgan joy menyudan tashqarida bo'lsa, menyuni yopish
+window.addEventListener("click", (e) => {
+  if (!headerMenu.contains(e.target) && !openMenuBtn.contains(e.target)) {
+    headerMenu.classList.add("-translate-x-full");
+    document.body.classList.remove("overflow-hidden", "pr-[5px]");
+  }
 });
 
 // interyer-nomer__swiper
 var swiper = new Swiper(".interyer-nomer__swiper", {
   slidesPerView: 4,
   spaceBetween: 51,
-  loop: true,
+  loop: false,
   speed: 700,
   navigation: {
     nextEl: ".next-btn",
@@ -282,3 +307,161 @@ try {
 } catch (error) {
   console.log(error);
 }
+
+try {
+  ymaps.ready(init);
+
+  function init() {
+    // Create a new map instance
+    var myMap = new ymaps.Map("map2", {
+      center: [44.895, 37.316], // Coordinates of the center of the map (Anapa)
+      zoom: 13,
+      controls: ["zoomControl", "fullscreenControl"],
+    });
+
+    myMap.setType("yandex#map");
+    myMap.geoObjects.options.set("preset", "islands#grayIcon");
+
+    var location1 = [44.89, 37.32];
+    var location2 = [44.9, 37.31];
+    var location3 = [44.91, 37.32];
+    var location4 = [44.92, 37.33];
+    var location5 = [44.93, 37.34];
+    var location6 = [44.93, 37.34];
+    var location7 = [44.93, 37.34];
+
+    var placemark1 = new ymaps.Placemark(
+      location1,
+      {},
+      {
+        iconLayout: "default#image",
+        iconImageHref: "../../public/assets/images/logo/company-logo.svg",
+        iconImageSize: [120, 120],
+        iconImageOffset: [-60, -60], // Adjust as needed
+      }
+    );
+
+    var placemark2 = new ymaps.Placemark(
+      location2,
+      {},
+      {
+        iconLayout: "default#image",
+        iconImageHref: "../../public/assets/images/icons/map-icon-1.svg",
+        iconImageSize: [34, 34],
+        iconImageOffset: [-17, -17], // Adjust as needed
+      }
+    );
+
+    var placemark3 = new ymaps.Placemark(
+      location3,
+      {},
+      {
+        iconLayout: "default#image",
+        iconImageHref: "../../public/assets/images/icons/map-icon-2.svg",
+        iconImageSize: [34, 34],
+        iconImageOffset: [10, -17], // Adjust as needed
+      }
+    );
+
+    var placemark4 = new ymaps.Placemark(
+      location4,
+      {},
+      {
+        iconLayout: "default#image",
+        iconImageHref: "../../public/assets/images/icons/map-icon-3.svg",
+        iconImageSize: [34, 34],
+        iconImageOffset: [-17, -17], // Adjust as needed
+      }
+    );
+
+    var placemark5 = new ymaps.Placemark(
+      location5,
+      {},
+      {
+        iconLayout: "default#image",
+        iconImageHref: "../../public/assets/images/icons/map-icon-4.svg",
+        iconImageSize: [34, 34],
+        iconImageOffset: [-17, 207], // Adjust as needed
+      }
+    );
+    var placemark6 = new ymaps.Placemark(
+      location6,
+      {},
+      {
+        iconLayout: "default#image",
+        iconImageHref: "../../public/assets/images/icons/map-icon-5.svg",
+        iconImageSize: [34, 34],
+        iconImageOffset: [107, 207],
+      }
+    );
+    var placemark7 = new ymaps.Placemark(
+      location7,
+      {},
+      {
+        iconLayout: "default#image",
+        iconImageHref: "../../public/assets/images/icons/map-icon-6.svg",
+        iconImageSize: [34, 34],
+        iconImageOffset: [257, 307],
+      }
+    );
+
+    // Add placemarks to the map
+    myMap.geoObjects.add(placemark1);
+    myMap.geoObjects.add(placemark2);
+    myMap.geoObjects.add(placemark3);
+    myMap.geoObjects.add(placemark4);
+    myMap.geoObjects.add(placemark5);
+    myMap.geoObjects.add(placemark6);
+    myMap.geoObjects.add(placemark7);
+  }
+} catch (error) {
+  console.log(error);
+}
+
+// oasis-swiper
+try {
+  var swiper = new Swiper(".oasis-swiper", {
+    slidesPerView: 2,
+    spaceBetween: 51,
+    speed: 900,
+    slidesPerGroup: 2,
+    navigation: {
+      nextEl: ".oasis-next-btn",
+      prevEl: ".oasis-prev-btn",
+    },
+    breakpoints: {
+      992: {
+        slidesPerView: 2,
+        spaceBetween: 51,
+      },
+      0: {
+        slidesPerView: 1.3,
+        spaceBetween: 23,
+        slidesPerGroup: 1,
+      },
+    },
+  });
+} catch (error) {}
+
+Fancybox.bind("[data-fancybox]", {});
+Fancybox.bind('[data-fancybox="gallery2"]', {});
+
+// product-swiper
+try {
+  var swiper = new Swiper(".product-swiper", {
+    direction: "vertical",
+    slidesPerView: 2,
+    spaceBetween: 0,
+    grid: {
+      rows: 2,
+      fill: "row",
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      draggable: true,
+    },
+    mousewheel: true,
+    freeMode: false,
+    slidesPerColumnFill: "row",
+  });
+} catch (error) {}
